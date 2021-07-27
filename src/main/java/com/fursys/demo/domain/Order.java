53f99091c13fb40d1c826332ab31bf6cc0b9cae7
hyunsu_han <hyunsu_han@fursys.com>
2021-07-27 14:25:35 +0900

    지연 로딩 및 연관관계 편의 메소드 추가.

--- src/main/java/com/fursys/demo/domain/Order.java
+++ src/main/java/com/fursys/demo/domain/Order.java
@@ -8,6 +8,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.FetchType.LAZY;
+
 @Entity
 @Table(name = "orders")
 @Getter
@@ -20,14 +22,14 @@ public class Order {
 
     private String name;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -35,4 +37,22 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // ORDER, CANCEL
+
+    /**
+     *  연관관계 편의 메소드, 양방향 관계일 때 사용하면 편리함.
+     */
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
