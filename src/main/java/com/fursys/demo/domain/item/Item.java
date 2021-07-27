5b7c4bfe1b5c2d886b3576b80711af10e3c9edde
hyunsu_han <hyunsu_han@fursys.com>
2021-07-27 17:12:10 +0900

    상품 비즈니스 로직 및 기능 추가

--- src/main/java/com/fursys/demo/domain/item/Item.java
+++ src/main/java/com/fursys/demo/domain/item/Item.java
@@ -1,6 +1,7 @@
 package com.fursys.demo.domain.item;
 
 import com.fursys.demo.domain.Category;
+import com.fursys.demo.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -25,4 +26,22 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    /**
+     *  재고 수량 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     *  재고 수량 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
